1fdf2fbff8c33044df7f689f3c1e0c68f306944e
帅得乱七八糟 <284970670@qq.com>
2021-11-12 21:42:43 +0800

    完成数据存放在localStorage 以及增添新计划功能

---
+++ src/components/Plan.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function Plan(props) {
+  const {
+    title,
+    startTime,
+    endTime,
+    instructions,
+    specificItems
+  } = props;
+  return (
+    <div className="plan">
+      <div className="plan__header">
+        <div className="plan__title">
+          {title}
+        </div>
+        {/* 注意，不是每一个div标签后面都需要 className */}
+        <div>
+          <button className="btn btn--primary mr-1">编辑</button>
+          <button className="btn btn--danger">删除</button>
+        </div>
+      </div>
+      <div className="plan__row">   {/* 这种row的格式非常不错！*/}
+        <span className="plan__label">开始时间：</span>
+        <span className="plan__value">{startTime}</span>
+      </div>
+      <div className="plan__row">   
+        <span className="plan__label">结束时间：</span>
+        <span className="plan__value">{endTime}</span>
+      </div>
+      <div className="plan__row">   
+        <span className="plan__label plan__label--special">备忘录：</span>
+        <div className="plan__value plan__value--special">{instructions}</div>
+      </div>
+      <div className="plan__row">   
+        <span className="plan__label plan__label--special">计划细节</span>
+        <div className="plan__value plan__value--special">{specificItems.map((item, index) => {
+          return <div key={item.id}>△{item.describe}</div>
+        })}</div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/PlanList.js
@@ -0,0 +1,27 @@
+import React, {useContext} from 'react';
+import Plan from './Plan';
+import { PlansContext } from './App';
+export default function PlanList({plans}) {
+
+  const { handlePlanAdd } = useContext(PlansContext);
+  return (
+    <div className="plan-list">
+      <div className="plan-list__title">
+        计划列表
+      </div>
+      <div>
+        {
+          plans.map((plan) => {
+            return <Plan key={plan.id} {...plan}/> // 这里是用的 {...plan} 解构哦~
+          })
+        }
+      </div>
+      <div className="plan-list__add-plan-btn-container">   {/* 这种命名才霸道嘛~ */}
+        <button 
+          className="btn btn--add"
+          onClick={() => handlePlanAdd()}
+        >增添新的计划</button>
+      </div>
+    </div>
+  )
+}
